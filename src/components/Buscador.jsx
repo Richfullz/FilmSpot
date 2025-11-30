1d719fe86409ff4095529502307469c3fe7569c5
Richfullz <alejandropascualsanchez@gmail.com>
2025-11-30 18:35:34 +0100

    filmSpot

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -3,15 +3,14 @@ import React, { useState } from "react";
 const Buscador = ({ listadoState, setListadoState }) => {
   const [busqueda, setBusqueda] = useState("");
   const [noEncontrado, setNoEncontrado] = useState(false);
-
   const buscarPeli = (e) => {
+
     setBusqueda(e.target.value);
 
-    let pelis_encontradas = listadoState.filter((peli) =>
-      peli.titulo.toLowerCase().includes(busqueda.toLowerCase())
-    );
-
-    if (busqueda.length <= 1 || pelis_encontradas.length === 0) {
+    let pelis_encontradas = listadoState.filter((peli) => {
+      return peli.titulo.toLowerCase().includes(busqueda.toLowerCase());
+    });
+    if (busqueda.length <= 1 || pelis_encontradas <= 0) {
       pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
       setNoEncontrado(true);
     } else {
@@ -20,24 +19,25 @@ const Buscador = ({ listadoState, setListadoState }) => {
 
     setListadoState(pelis_encontradas);
   };
-
   return (
-    <div className="search">
-      <h3 className="title">Buscador</h3>
-      {noEncontrado && busqueda.length > 1 && (
-        <span className="no-encontrado">No hay resultados</span>
-      )}
-      <form>
-        <input
-          type="text"
-          id="search_field"
-          value={busqueda}
-          autoComplete="off"
-          onChange={buscarPeli}
-        />
-        <button id="search">Buscar</button>
-      </form>
-    </div>
+    <>
+      <div className="search">
+        <h3 className="title">Buscador</h3>
+        {noEncontrado == true && busqueda.length > 1 && (
+          <span className="no-encontrado">No hay resultados</span>
+        )}
+        <form>
+          <input
+            type="text"
+            id="search_field"
+            value={busqueda}
+            autoComplete="off"
+            onChange={buscarPeli}
+          />
+          <button id="search">Buscar</button>
+        </form>
+      </div>
+    </>
   );
 };
 
